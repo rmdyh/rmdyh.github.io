2021de74d7600f433c1f889990adecc32454e0b1
rmdyh <31561885+rmdyh@users.noreply.github.com>
2017-09-09 15:00:29 +0800

    1page

--- js/main.js
+++ js/main.js
@@ -1,16 +1,24 @@
 (function(){
+	h = window.innerHeight;
+	w = window.innerWidth;
 	bgChange();
+	titleChange();
+	listChange();
 })();
+
+var h;
+var w;
 
 //监听窗口改变
 window.onresize = function(){
+	h = window.innerHeight;
+	w = window.innerWidth;
 	bgChange();
+	titleChange();
+	listChange();
 }
 
 function bgChange(){
-	var h = window.innerHeight;
-	var w = window.innerWidth;
-
 	var bg = document.getElementsByClassName("page-bg");
 
 	if((w/h) >= (1920/1370)){
@@ -29,4 +37,35 @@ function bgChange(){
             bg[i].style.top = '0';
         }
     }
+}
+
+function titleChange(){
+	var ttl = document.getElementById("title").style;
+	var navimg = document.getElementById("navimg").style;
+
+	if(w < 480){
+		ttl.fontSize = w/20 + 'px';
+		ttl.marginTop = 34 - (w / 20) +'px';
+		navimg.width = w/12 + 'px';
+		navimg.height = w/12 + 'px';
+		navimg.top = 35 - w/24 + 'px'; 
+	}
+	else{
+		ttl.fontSize = "24px";
+		ttl.marginTop = "10px";
+		navimg.width = "40px";
+		navimg.height = "40px";
+		navimg.top = "15px";
+	}
+}
+
+var status=1;
+//status=0 welcome
+//status=1 list
+//status=2 article
+function listChange(){
+//	if(status !=1) return;
+	var conlist = document.getElementById("content-list").style;
+
+	conlist.height = h - 70 + 'px';
 }
